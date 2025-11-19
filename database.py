0326d39e21b1fc76142beb33bc2363ec9b5a0438
quintaelombu <jegaldeano46@gmail.com>
2025-11-19 19:50:53 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Usamos SQLite local (no tenés que pagar nada)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./mediturnos.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
