54493658db408611fe5349daee4b1f96813f6519
quintaelombu <jegaldeano46@gmail.com>
2025-11-20 19:48:32 -0300

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,14 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
