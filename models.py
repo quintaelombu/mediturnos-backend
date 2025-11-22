b0b865824cd88690010c25926b89743b4459a5bc
quintaelombu <jegaldeano46@gmail.com>
2025-11-22 19:51:59 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,28 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
-class Medic(Base):
-    __tablename__ = "medics"
+
+class Doctor(Base):
+    __tablename__ = "doctores"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    specialty = Column(String(100), nullable=False)
-    price = Column(Integer, nullable=False)
-    duration_minutes = Column(Integer, nullable=False)
+    nombre = Column(String, nullable=False)
+    especialidad = Column(String, nullable=False)
+    duracion_turno = Column(Integer, nullable=False)
+    precio = Column(Integer, nullable=False)
+
+    turnos = relationship("Turno", back_populates="doctor")
+
+
+class Turno(Base):
+    __tablename__ = "turnos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    doctor_id = Column(Integer, ForeignKey("doctores.id"))
+    paciente_nombre = Column(String, nullable=False)
+    paciente_email = Column(String, nullable=False)
+    fecha = Column(Date, nullable=False)
+    hora = Column(Time, nullable=False)
+
+    doctor = relationship("Doctor", back_populates="turnos")
