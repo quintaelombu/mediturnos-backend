2024ba81e0054b8d517b7a5a9ce674b441837c7b
quintaelombu <jegaldeano46@gmail.com>
2025-11-22 22:02:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,19 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import Base, engine, get_db
-import mercadopago
-
-from models import Doctor, Turno
-from schemas import DoctorCreate, Doctor, TurnoCreate, Turno
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
-app = FastAPI()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-Base.metadata.create_all(bind=engine)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-mp = mercadopago.SDK(os.getenv("MP_ACCESS_TOKEN"))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
 
-@app.get("/")
-def root():
-    return {"status": "backend ok"}
-
-
-@app.post("/doctores", response_model=Doctor)
-def crear_doctor(data: DoctorCreate, db: Session = Depends(get_db)):
-    nuevo = Doctor(**data.dict())
-    db.add(nuevo)
-    db.commit()
-    db.refresh(nuevo)
-    return nuevo
-
-
-@app.get("/doctores")
-def listar_doctores(db: Session = Depends(get_db)):
-    return db.query(Doctor).all()
-
-
-@app.post("/turnos", response_model=Turno)
-def crear_turno(data: TurnoCreate, db: Session = Depends(get_db)):
-    nuevo = Turno(**data.dict())
-    db.add(nuevo)
-    db.commit()
-    db.refresh(nuevo)
-    return nuevo
-
-
-@app.post("/mp/preferencia")
-def crear_preferencia(data: TurnoCreate):
-    preference_data = {
-        "items": [{
-            "title": f"Consulta médica con ID doctor {data.doctor_id}",
-            "quantity": 1,
-            "currency_id": "ARS",
-            "unit_price": 15000
-        }],
-        "payer": {
-            "name": data.paciente_nombre,
-            "email": data.paciente_email
-        },
-        "back_urls": {
-            "success": "https://mediturnos-frontend-production.up.railway.app/success",
-            "pending": "https://mediturnos-frontend-production.up.railway.app/pending",
-            "failure": "https://mediturnos-frontend-production.up.railway.app/error"
-        }
-    }
-
-    res = mp.preference().create(preference_data)
-    return res
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
